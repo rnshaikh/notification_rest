43f04e7384542b8c85b1a32fbfcdd36928cc8357
Rizwan Shaikh <rshaikh@ces-ltd.com>
2020-02-28 19:27:11 +0530

    heroku settings

--- notification_poc/settings.py
+++ notification_poc/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'notification_poc.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
     # 'default': {
