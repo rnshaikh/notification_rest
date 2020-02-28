a039ad54456e86f3f68bb6db50ad7ea0f3635a58
Rizwan Shaikh <rshaikh@ces-ltd.com>
2020-02-28 19:56:34 +0530

    added push

--- notification_poc/settings.py
+++ notification_poc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm-f7&h775#be^k&=4!905_)#em)w198baaf1kjy01v)^dk1b#n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
